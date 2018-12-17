2d6d22b32b672f916389257748697c7e22b24004
Nambiar <simithn@186590d21625.ant.amazon.com>
2018-12-18 00:20:00 +0800

    Added header

--- app/src/main/java/com/wearables/ge/wearables_ble_receiver/utils/MqttManager.java
+++ app/src/main/java/com/wearables/ge/wearables_ble_receiver/utils/MqttManager.java
@@ -1,71 +1,131 @@
+/*-------------------------------------------------------------------------------
+# Copyright (c) 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This source code is subject to the terms found in the AWS Enterprise Customer Agreement.
+#-------------------------------------------------------------------------------*/
 package com.wearables.ge.wearables_ble_receiver.utils;
 
 import android.content.Context;
 import android.util.Log;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobile.auth.core.IdentityManager;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
-import com.amazonaws.regions.Regions;
+
+import org.json.JSONObject;
 
 import java.util.UUID;
 
-/**
- * Created by simithn on 23/11/18.
- */
 
 public class MqttManager {
 
    String mClientId;
    Context mCtxt;
    String mAwsIoTEndpoint;
+   AWSCredentialsProvider mCredentialsProvider;
+   private static AWSIotMqttManager mMqttMgr;
+   private static MqttManager mInstance;
 
-    AWSCredentialsProvider mCredentialsProvider;
-    // Customer specific IoT endpoint
-    //private static final String CUSTOMER_SPECIFIC_ENDPOINT = "a1bb7j6i5uiivh-ats.iot.us-west-2.amazonaws.com";
-    private static final String CUSTOMER_SPECIFIC_ENDPOINT = "a3bx6ijnr5v29n-ats.iot.us-east-1.amazonaws.com";
+   public enum ConnectionStatus{
+        NOT_CONNECTED,CONNECTING,CONNECTED,RECONNECTING,DISCONNECTED
+    };
 
-    private static AWSIotMqttManager mMqttMgr;
+   ConnectionStatus mConnectionStatus = ConnectionStatus.NOT_CONNECTED;
 
-   //TODO : modularise with endpoint etc.
-   public MqttManager(Context pCtxt){
+   private final static String TAG = "MqttManager";
 
+   private MqttManager(Context pCtxt){
        mCtxt = pCtxt;
+       mAwsIoTEndpoint = getConfig("IoTEndpoint");
        mClientId = UUID.randomUUID().toString();
-       mMqttMgr = new AWSIotMqttManager(mClientId, CUSTOMER_SPECIFIC_ENDPOINT);
+       mMqttMgr = new AWSIotMqttManager(mClientId, mAwsIoTEndpoint);
        mCredentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
-
    }
 
-   public void connect(){
+    public static MqttManager getInstance(Context pCtxt){
 
+        if(mInstance == null){
+            mInstance = new MqttManager(pCtxt);
+        }
+        return mInstance;
+    }
+
+
+    private String getConfig(String pKey){
+
+       String value = null;
+       AWSConfiguration configuration = IdentityManager.getDefaultIdentityManager().getConfiguration();
+       JSONObject object = configuration.optJsonObject("IoTConfig");
        try {
-           mMqttMgr.connect(mCredentialsProvider, new AWSIotMqttClientStatusCallback() {
-               @Override
-               public void onStatusChanged(final AWSIotMqttClientStatus status,
-                                           final Throwable throwable) {
-                   Log.d("D","Status"+status);
-               }
-           });
+           value = (String) object.get(pKey);
        }
        catch(Exception e){
            e.printStackTrace();
        }
+
+       return value;
    }
 
+    public void connect(){
 
+        //TODO: maintain status of the Connection and provide it to the Application if required
 
-   //TODO: can add supported QoS as input parameter
-   public static void publish(String pMqttTopic,String pMsg){
-       try {
-           mMqttMgr.publishString( pMsg,pMqttTopic, AWSIotMqttQos.QOS0);
-       }
-       catch(Exception e){
-           e.printStackTrace();
-       }
-   }
+        try {
+            mMqttMgr.connect(mCredentialsProvider, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status,
+                                            final Throwable throwable) {
+                    Log.d(TAG,"Status"+status);
 
+                    switch(status){
+
+                        case Connecting:{
+
+                            mConnectionStatus = ConnectionStatus.CONNECTING;
+                        }
+                        break;
+
+                        case Connected:{
+                            mConnectionStatus = ConnectionStatus.CONNECTED;
+
+                        }
+                        break;
+
+                        case Reconnecting:{
+                            mConnectionStatus = ConnectionStatus.RECONNECTING;
+                        }
+                        break;
+
+                        case ConnectionLost:{
+                            mConnectionStatus = ConnectionStatus.DISCONNECTED;
+                        }
+                        break;
+                    }
+                }
+            });
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public ConnectionStatus getConnectionStatus(){
+
+        return mConnectionStatus;
+    }
+
+    //TODO for the next phase and provide callbacks for Message delivery notifications:
+    // can add supported QoS as input parameter, callback function
+    public  void publish(String pMqttTopic,String pMsg){
+        try {
+            mMqttMgr.publishString( pMsg,pMqttTopic, AWSIotMqttQos.QOS0);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
